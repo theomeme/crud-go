2fb65b53ae7450d155ecaaa167dc80d269c41dd9
Theodoro <theomeme21@gmail.com>
2023-12-14 10:59:44 -0300

    Create Request and validation

---
+++ src/configuration/validation/validate_user.go
@@ -0,0 +1,23 @@
+package validation
+
+import (
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translation "github.com/go-playground/validator/v10/translations/en"
+)
+
+var (
+	Validate = validator.New()
+	transl   ut.Translator
+)
+
+func init() {
+	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		en := en.New()
+		unt := ut.New(en, en)
+		transl, _ = unt.GetTranslator("en")
+		en_translation.RegisterDefaultTranslations(val, transl)
+	}
+}
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,13 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/theomeme/crud-go/src/configuration/rest_err"
+	"github.com/theomeme/crud-go/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error %s \n", err.Error()),
+		)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
